d5c848ee19f768f015061f992a2dbabe23ce3c8d
McCowsky <makowskimateusz@outlook.com>
2022-12-05 05:36:17 +0100

    fix serviceworker

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,7 @@ import dataset from "../dataset";
 import * as uuid from "uuid";
 
 export const handlers = [
-  rest.get("http://api.trello-clone.com/api/columns", (req: any, res: any, ctx: any) => {
+  rest.get("https://api.trello-clone.com/api/columns", (req: any, res: any, ctx: any) => {
     return res(
       ctx.status(200),
       ctx.json({
@@ -27,27 +27,30 @@ export const handlers = [
     );
   }),
 
-  rest.post("https://api.trello-clone.com/api/columns", (req: any, res: any, ctx: any) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        [uuid.v4()]: {
-          name: "Requested",
-          items: [],
-        },
-        [uuid.v4()]: {
-          name: "To do",
-          items: [],
-        },
-        [uuid.v4()]: {
-          name: "In Progress",
-          items: [],
-        },
-        [uuid.v4()]: {
-          name: "Done",
-          items: [],
-        },
-      })
-    );
-  }),
+  rest.post(
+    "https://api.trello-clone.com/api/columns",
+    (req: any, res: any, ctx: any) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          [uuid.v4()]: {
+            name: "Requested",
+            items: [],
+          },
+          [uuid.v4()]: {
+            name: "To do",
+            items: [],
+          },
+          [uuid.v4()]: {
+            name: "In Progress",
+            items: [],
+          },
+          [uuid.v4()]: {
+            name: "Done",
+            items: [],
+          },
+        })
+      );
+    }
+  ),
 ];
